7c449aeabdc7443d1e1ff46c7c404d27eb0c1fd6
Justin Mitchell <justin.mitchell@auspost.com.au>
2020-10-02 16:31:06 +1000

    Added enzyme tests

--- src/components/name/name.test.js
+++ src/components/name/name.test.js
@@ -1,50 +1,31 @@
-import { act } from "react-dom/test-utils";
 import { Name } from "./name";
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 
 describe("Component: Name", () => {
 
-  describe("The native React way", () => {
-    let container;
+  let component = null;
 
-    beforeEach(() => {
-      container = document.createElement('div');
-      document.body.appendChild(container);
-    });
-
-    afterEach(() => {
-      document.body.removeChild(container);
-      container = null;
-    });
-
-    it("should render component", () => {
-      act(() => {
-        ReactDOM.render(<Name />, container);
-      });
-
-      const outString = container.querySelector("#output");
-      expect(outString.textContent).toBe("Hello, ");
-    });
-
-    it("should update component when user types in name", () => {
-      act(() => {
-        ReactDOM.render(<Name />, container);
-      });
-
-      const outString = container.querySelector("#output");
-      const inputEle = container.querySelector("#name");
-
-      act(() => {
-        var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
-        nativeInputValueSetter.call(inputEle, 'Foo');
-
-        var ev2 = new Event('input', { bubbles: true });
-        inputEle.dispatchEvent(ev2);
-      });
-      
-      expect(outString.textContent).toBe("Hello, Foo");
-    })
+  beforeEach(() => {
+    component = shallow(<Name />);
   })
+
+  afterEach(() => {
+    component = null;
+  });
+
+  it("should render component", () => {
+    expect(component.exists("form")).toBe(true);
+  });
+
+  it("should render default state", () => {
+    expect(component.exists("#output")).toBe(true);
+    expect(component.find("#output").text()).toBe("Hello, ");
+  });
+
+  it("should display name when user types into input field", () => {
+    component.find("input#name").simulate("change", { target: { value: "Charles" } });
+    expect(component.find("#output").text()).toBe("Hello, Charles");
+  });
 
 });
